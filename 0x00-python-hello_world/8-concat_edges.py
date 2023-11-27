a633a456f312de211241efc976ecd643999bb722
sozoia <zfzg2232@gmail.com>
2023-11-27 20:53:35 +0300

    task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# i don't know the hell what is going on
-print(f"{str[39:66]} {str[107:111]} {str[:6]}")
+print(str[39:67], str[107:112], str[:6])
+# ex
