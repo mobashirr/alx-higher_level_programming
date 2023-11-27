3d586a3d9b52227274bc10375d7c16ee34d4e025
sozoia <zfzg2232@gmail.com>
2023-11-27 18:49:07 +0300

    task 4

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 number = 3.14159
-print(f"Float: {round(number,2)}")	#round fuction used to round a number to a specified number of digits.
+print(f"Float: {round(number,2)}")	
+#round fuction used to round a number to a specified number of digits.
