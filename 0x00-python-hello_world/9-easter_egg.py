3b635f543b043f3cadba0de93002701ca0af90b7
sozoia <zfzg2232@gmail.com>
2023-11-27 21:13:21 +0300

    task 9

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import io
+import contextlib
+
+with io.StringIO() as buf, contextlib.redirect_stdout(buf):
+    import this
+    zen = buf.getvalue()
+
+# Print the Zen of Python
+print(f"{zen[:34]}\n{zen[35:]}")
