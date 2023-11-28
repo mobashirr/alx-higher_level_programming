1f79c58c8db058ce9983945a826899c5341ba3b9
sozoia <zfzg2232@gmail.com>
2023-11-28 19:54:15 +0300

    task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+
+def pow(a, b):
+    
+    result = a
+
+    if b == 0:
+        return (1)
+
+    if b > 0:
+        for _ in range(1, b):
+            result *= a
+    elif b < 0:
+        b *= -1
+        result = 1 / result
+        for _  in range(1, b + 1):
+            result /= 10
+
+    return(result)
+
+
+pow = __import__('11-pow').pow
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
