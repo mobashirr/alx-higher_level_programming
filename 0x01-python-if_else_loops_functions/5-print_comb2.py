6a1c1fabcddf1aeae60b44b8773e3f538283b78f
sozoia <zfzg2232@gmail.com>
2023-11-28 15:12:16 +0300

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    #we use cnditional expretion
-    #in order to ensure last digit with no sepreate
-    #and :02d ensure the format is only two digits
+#we use cnditional expretion
+#in order to ensure last digit with no sepreate
+#and :02d ensure the format is only two digits
+#flush is good practise
     print("{:02d}".format(i), end=", " if i < 99 else "\n", flush=True)
