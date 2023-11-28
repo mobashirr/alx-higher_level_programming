d2398deb5135fa08ca0e92f936b24cfa949cc22f
sozoia <zfzg2232@gmail.com>
2023-11-28 15:31:22 +0300

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         # this way no duplcated couple will shown
-	#also no sep at the end
-        print("{:d}{:d}".format(i, j), end=", " if i != 8 else "\n", flush=True)
+        # also no sep at the end
+        print("{:d}{:d}".format(i, j), end=", " if i != 8 else "\n")
