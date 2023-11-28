5aca7ebe12002375d83e8fa28f2e286719ed8671
sozoia <zfzg2232@gmail.com>
2023-11-28 16:22:00 +0300

    taskk 7 pycodesttyle

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,15 +4,17 @@ def islower(c):
 
     result = False
 
-    if(c == ""):
-        return(result)
+    if (c == ""):
+        return (result)
     i = int.from_bytes(c.encode('utf-8'), 'big')
 
     for j in range(97, 123):
         if i == j:
-            return(True)
+            return (True)
 
-    return(result)
+    return (result)
+
+
 '''
 islower = __import__('7-islower').islower
 
