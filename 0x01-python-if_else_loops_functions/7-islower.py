48c9e0b88c346029c82022c1d2d632e606a7077e
sozoia <zfzg2232@gmail.com>
2023-11-28 15:46:52 +0300

    task 7 first try

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,10 +3,19 @@
 def islower(c):
 
     result = False
-    i = f"{c:d}"
+    i = int.from_bytes(c.encode('utf-8'), 'big')
 
     for j in range(97, 123):
         if i == j:
             return(True)
 
     return(result)
+'''
+islower = __import__('7-islower').islower
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper")
+'''
