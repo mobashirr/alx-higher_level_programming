4d470c692c60458194c73d8e13a08b78f17997dd
sozoia <zfzg2232@gmail.com>
2023-11-28 18:55:30 +0300

    task 9 first

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+def print_last_digit(number):
+    
+    last = number % 10
+
+    if(number < 0):
+        last = number % -10
+        last *= -1
+
+    print(last, end="")
+    return (last)
+
+
+'''
+print_last_digit = __import__('9-print_last_digit').print_last_digit
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
+'''
