8b76f13be1e3a67ac09da77803ff41b9de399faa
sozoia <zfzg2232@gmail.com>
2023-11-28 16:18:35 +0300

    try if statment

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,6 +3,9 @@
 def islower(c):
 
     result = False
+
+    if(c == ""):
+        return(result)
     i = int.from_bytes(c.encode('utf-8'), 'big')
 
     for j in range(97, 123):
@@ -17,5 +20,6 @@ print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
 print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper")
+print("g is {}".format("lower" if islower("g") else "upper"))
+print("notiong is {}".format("lower" if islower("") else "upper"))
 '''
