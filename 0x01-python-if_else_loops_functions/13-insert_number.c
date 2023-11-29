d46e0e89eecd99a865e405f9ffe7a6778de3ff91
sozoia <zfzg2232@gmail.com>
2023-11-29 23:09:33 +0300

    task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,14 +2,11 @@
 
 listint_t *add(listint_t **head, int num)
 {
-	listint_t *temp, *new, *next;
+	listint_t *temp = NULL, *new, *next;
 	/*set if no list or if head->n less that number*/
 
 			if((*head))
 				temp = (*head);
-
-
-
 
 			/*set he new node:*/
 			new = malloc(sizeof(listint_t));
@@ -17,11 +14,13 @@ listint_t *add(listint_t **head, int num)
 			if(!new)
 				return(NULL);
 			new->n = num;	/*set the value*/
+
 			if(temp)
 			{
-				if (temp->n > num)
+				if (temp->n > num)		/*case insert at first num < first elment*/
 				{
 					new->next = temp;
+					(*head) = new;		/*update the head required*/
 					return(new);
 				}
 				if(temp->next)
@@ -33,6 +32,8 @@ listint_t *add(listint_t **head, int num)
 				}
 				return(temp);
 			}
+			new->next = NULL;
+			(*head) = new;
 			return(new);	/*return the new node*/
 }
 listint_t *insert_node(listint_t **head, int number)
@@ -78,27 +79,24 @@ int main(void)
     listint_t *head;
 
     head = NULL;
-	
+
     add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 2);
     add_nodeint_end(&head, 3);
     add_nodeint_end(&head, 4);
-   add_nodeint_end(&head, 28);
-    add_nodeint_end(&head, 28);
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
     print_listint(head);
 
     printf("-----------------\n");
 
-    insert_node(&head, 27);
-
+    insert_node(&head, -7);
+	add_nodeint_end(&head, 3);
     print_listint(head);
 
     free_listint(head);
 
     return (0);
 }
-
 */
