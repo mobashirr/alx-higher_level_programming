eb590391a31a10d8effc5c9c4f69ca2fe2830402
sozoia <zfzg2232@gmail.com>
2023-11-30 14:52:37 +0300

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from sys import argv
+
+    len = len(argv)
+    cont = 0
+
+    for i in range(1, len):
+        cont += int(argv[i])
+    print(cont)
