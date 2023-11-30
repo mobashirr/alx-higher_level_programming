bb22a341f271d4c1faa2609fd045bf1f86c38aed
sozoia <zfzg2232@gmail.com>
2023-11-30 18:26:35 +0300

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if args_length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
+
     a = int(argv[1])
     b = int(argv[3])
     op = argv[2]
