b17d82291523465643bbaedac0f228401b496b24
sozoia <zfzg2232@gmail.com>
2023-11-30 14:42:09 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+
+    from sys import argv
+
+    len = len(argv)
+    print("{0} arguments.".format(len - 1))
+
+    for i in range(1, len):
+        print("{:d}: {:s}".format(i, argv[i]))
