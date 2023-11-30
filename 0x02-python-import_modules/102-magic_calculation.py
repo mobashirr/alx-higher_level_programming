134bb5fec93c0591e829da965c623ed7bcf74bf1
sozoia <zfzg2232@gmail.com>
2023-11-30 20:15:21 +0300

    task 103

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+	def magic_calculation(a, b):
+		
--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,1 +1,2 @@
 #!/usr/bin/python3
+(lambda f, x: print(f(f, x)))(lambda self, c: chr(c) + self(self, c + 1) if c < ord('Z') else chr(c), ord('A'))
