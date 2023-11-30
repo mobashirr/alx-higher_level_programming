eb854294d11acf5d710f2da1850f8652546a80ef
sozoia <zfzg2232@gmail.com>
2023-11-30 15:33:58 +0300

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import dis
+import hidden_4
+
+if __name__ == "__main__":
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    sorted_names = sorted(names)
+
+    for name in sorted_names:
+        print(name)
