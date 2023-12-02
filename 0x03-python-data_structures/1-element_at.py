da9bcc047eb7ff89d71ecb58b5ded579cc00867c
sozoia <zfzg2232@gmail.com>
2023-12-02 22:38:33 +0300

    fix range

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
 
-    i = len(my_list)
+    i = len(my_list) - 1
 
-    if idx > i or idx <= 0 :
+    if idx > i or idx < 0 :
         return (None)
     
     return (my_list[idx])
