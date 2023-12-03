d7992002af919e32481708712aa83e27e244bea4
sozoia <zfzg2232@gmail.com>
2023-12-03 14:47:54 +0300

    style

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,20 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    for j in range(len(matrix)):
-        for k in matrix[j]:
-            sep = " " if k != matrix[j][-1] else ""
-            print("{:d}".format(k), end="{}".format(sep))
-        print("$")
+    if not matrix:
+        pass
+    else:
+        for j in range(len(matrix)):
+            for k in matrix[j]:
+                sep = " " if k != matrix[j][-1] else ""
+                print("{:d}".format(k), end="{}".format(sep))
+            print("$")
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
