50d0f7db3ba3658a6ea8bae5612bd55db5e771c9
sozoia <zfzg2232@gmail.com>
2023-12-03 15:23:40 +0300

    fix condtions

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,14 +6,20 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     # set values for cases out of range
     a = tuple_a[0] if i >= 1 else 0
-    b = tuple_a[1] if i >= 1 else 0
-    a += tuple_a[0] if j >= 1 else 0
-    b += tuple_a[1] if j >= 1 else 0
-    '''
-    if i > 1 and j >= 1:
-        a = tuple_a[0] + tuple_b[0]
-    if i > 2 and j >= 2:
-        b = tuple_a[1] + tuple_b[1]
-    '''
+    b = tuple_a[1] if i >= 2 else 0
+    a += tuple_b[0] if j >= 1 else 0
+    b += tuple_b[1] if j >= 2 else 0
+
     tuple = (a, b)
     return (tuple)
+
+'''
+# test cases:
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
+'''
