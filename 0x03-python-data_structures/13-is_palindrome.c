6b7d7031289817a12b7d335498438932546ff381
sozoia <zfzg2232@gmail.com>
2023-12-03 21:35:03 +0300

    style

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,109 +2,80 @@
 
 void rev(int **arr, int len)
 {
-    int i,j,k;
-    k = len / 2;
+	int i,j,k;
+	k = len / 2;
 
-    for(i = 0; i < k; i++)
-    {
-        j = *arr[i];
-        *arr[i] = *arr[len];
-        *arr[len] = j;
-        ++i;
-        --len;
-    }
+	for(i = 0; i < k; i++)
+	{
+		j = *arr[i];
+		*arr[i] = *arr[len];
+		*arr[len] = j;
+		++i;
+		--len;
+	}
 }
 
 int count(listint_t *head)
 {
-    listint_t *temp;
+	listint_t *temp;
 
-    if(!head)
-        return(0);
+	if(!head)
+		return(0);
 
-    temp = head;
+	temp = head;
 
-    int i = 0;
-    while(temp)
-    {
-        if(temp->next)
-            temp = temp->next;
-        else
-            temp = NULL;
-        ++i;
-    }
-    /*printf("len=%d\n",i); */
-    return(i);
+	int i = 0;
+	while(temp)
+	{
+		if(temp->next)
+			temp = temp->next;
+		else
+			temp = NULL;
+		++i;
+	}
+	/*printf("len=%d\n",i); */
+	return(i);
 }
 
 int is_palindrome(listint_t **head)
 {
-    int len = 0, *arr,i = 0,j;
-    int pal = 0;    /*palndrome*/
-    listint_t *temp;
+	int len = 0, *arr,i = 0,j;
+	int pal = 0;    /*palndrome*/
+	listint_t *temp = NULL;
 
-    if (!(*head))
-        return(1);
+	if (!(*head))
+		return(1);
 
-    temp = (*head);
-    len = count(temp);
-    arr = malloc(sizeof(int) * (len / 2) );
+	temp = (*head);
+	len = count(temp);
+	arr = malloc(sizeof(int) * (len / 2) );
 
-    while(i < len/2 && temp)
-    {   /*save the half of elements*/
-        arr[i] = temp->n;
-        if (temp->next)
-            temp = temp->next;
-        else
-            break;
-        i++;
-    }
-    rev(&arr, len / 2);
+	while(i < len/2 && temp)
+	{   /*save the half of elements*/
+		arr[i] = temp->n;
+		if (temp->next)
+			temp = temp->next;
+		else
+			break;
+		i++;
+	}
+	rev(&arr, len / 2);
 
-    if (len % 2 != 0 && temp->next)
-        temp = temp->next;
+	if (len % 2 != 0 && temp->next)
+		temp = temp->next;
 
-    for (j = 0; temp; j++)
-    {
-            if(temp->n != arr[j])
-            {
-                pal = 1;
-                break;
-            }
-        if(temp->next)
-            temp = temp->next;
-        else
-        temp = NULL;
-    }
-    return(pal);
+	for (j = 0; temp; j++)
+	{
+			if(temp->n != arr[j])
+			{
+				pal = 1;
+				break;
+			}
+		if(temp->next)
+			temp = temp->next;
+		else
+		temp = NULL;
+	}
+	return(pal);
 }
 
-/*
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-   add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 320); 
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 1);
-    print_listint(head);
-
-    if (is_palindrome(&head) == 1)
-        printf("Linked list is a palindrome\n");
-    else
-        printf("Linked list is not a palindrome\n");
-
-    free_listint(head);
-
-    return (0);
-}
-*/
