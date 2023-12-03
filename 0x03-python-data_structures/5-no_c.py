4e687c2fdffc58e6008bea19a7880ed34a809ec5
sozoia <zfzg2232@gmail.com>
2023-12-03 13:58:39 +0300

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 
 def no_c(my_string):
 
-    str = "{}".format("".join(i for i in my_string if i != 'c'))
+    str = "{}".format("".join(i for i in my_string if (i != 'C' and i != 'c')))
     print(str)
-
     return(str)
+
