004e3ee3ceadfc8b5e1a6b141fe062edbd8f7f8e
sozoia <zfzg2232@gmail.com>
2023-12-03 15:32:02 +0300

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+	if len(sentence) == 0 or not sentence:
+		return(0, None)
+
+	c = sentence[0]
+	return (len(sentence), c)
+
+'''
+# test case:
+sentence = "At school, I learnt C!"
+length, first = multiple_returns("")
+print("Length: {:d} - First character: {}".format(length, first))
+'''
