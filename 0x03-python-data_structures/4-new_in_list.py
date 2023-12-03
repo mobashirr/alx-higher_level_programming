b4d14139bcd7a02a842a2844221a552e43d38932
sozoia <zfzg2232@gmail.com>
2023-12-03 11:21:21 +0300

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    i = len(my_list) - 1
+    new = [e for e in my_list]
+
+    if idx > i or idx < 0:
+        return (new)
+
+    new[idx] = element 
+    return(new)
