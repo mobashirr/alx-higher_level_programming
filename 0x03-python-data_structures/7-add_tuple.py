a3c95c5696e9120b3e532ea93dc56eddd1b0489d
sozoia <zfzg2232@gmail.com>
2023-12-03 15:17:49 +0300

    change the logic

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,13 +7,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # set values for cases out of range
     a = tuple_a[0] if i >= 1 else 0
     b = tuple_a[1] if i >= 1 else 0
-    a = tuple_a[0] if j >= 1 else 0
-    b = tuple_a[1] if j >= 1 else 0
-
+    a += tuple_a[0] if j >= 1 else 0
+    b += tuple_a[1] if j >= 1 else 0
+    '''
     if i > 1 and j >= 1:
         a = tuple_a[0] + tuple_b[0]
     if i > 2 and j >= 2:
         b = tuple_a[1] + tuple_b[1]
-
+    '''
     tuple = (a, b)
     return (tuple)
