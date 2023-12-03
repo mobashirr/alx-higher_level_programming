6508cba5bbfb815422d8017688af38ee9c8e9ea3
sozoia <zfzg2232@gmail.com>
2023-12-03 14:25:02 +0300

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+
+	for j in range(len(matrix)):
+		for k in matrix[j]:
+			print("{:d}".format(k),
+		 			end=" " if k !=matrix[j][-1] else "")
+		print("$")
