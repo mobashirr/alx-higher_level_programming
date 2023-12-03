f800e891b0833efc60754fe594498306dbff248b
sozoia <zfzg2232@gmail.com>
2023-12-03 15:33:19 +0300

    style

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,11 +2,12 @@
 
 def multiple_returns(sentence):
 
-	if len(sentence) == 0 or not sentence:
-		return(0, None)
+    if len(sentence) == 0 or not sentence:
+        return (0, None)
 
-	c = sentence[0]
-	return (len(sentence), c)
+    c = sentence[0]
+    return (len(sentence), c)
+
 
 '''
 # test case:
