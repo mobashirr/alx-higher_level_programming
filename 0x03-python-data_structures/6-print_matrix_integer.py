a531a2aed3358d4288c61be2c0d8c214bd003d93
sozoia <zfzg2232@gmail.com>
2023-12-03 14:34:09 +0300

    style

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-
-	for j in range(len(matrix)):
-		for k in matrix[j]:
-			print("{:d}".format(k),
-		 			end=" " if k !=matrix[j][-1] else "")
-		print("$")
+    for j in range(len(matrix)):
+        for k in matrix[j]:
+            sep = " " if k != matrix[j][-1] else ""
+            print("{:d}".format(k), end="{}".format(sep))
+        print("$")
