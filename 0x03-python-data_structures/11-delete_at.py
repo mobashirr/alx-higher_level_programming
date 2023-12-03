0c5576466df107bdc5020d8f1c7e7e0666c73cc6
sozoia <zfzg2232@gmail.com>
2023-12-03 17:15:30 +0300

    style and length

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    i = len(my_list)
+    i = len(my_list) - 1
 
     if idx > i or idx < 0:
         return (my_list)
 
     del my_list[idx]
-    return(my_list)
+    return (my_list)
 
 
 '''
