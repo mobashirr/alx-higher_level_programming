b7e550303763518789495877b6f2f0ea74435c65
sozoia <zfzg2232@gmail.com>
2023-12-03 15:13:55 +0300

    style and condition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-	i = len(tuple_a)
-	j = len(tuple_b)
+    i = len(tuple_a)
+    j = len(tuple_b)
 
-	a = tuple_a[0] if i >= 1 else 0
-	b = tuple_a[1] if i >= 1 else 0
-	a = tuple_a[0] if j >= 1 else 0
-	b = tuple_a[1] if j >= 1 else 0
+    # set values for cases out of range
+    a = tuple_a[0] if i >= 1 else 0
+    b = tuple_a[1] if i >= 1 else 0
+    a = tuple_a[0] if j >= 1 else 0
+    b = tuple_a[1] if j >= 1 else 0
 
-	if i > 1 and j >= 1:
-		a = tuple_a[0] + tuple_b[0]
-	if i > 1 and j >= 2:
-		b = tuple_a[1] + tuple_b[1]
+    if i > 1 and j >= 1:
+        a = tuple_a[0] + tuple_b[0]
+    if i > 2 and j >= 2:
+        b = tuple_a[1] + tuple_b[1]
 
-	tuple = (a, b)
-	return (tuple)
-
-
+    tuple = (a, b)
+    return (tuple)
