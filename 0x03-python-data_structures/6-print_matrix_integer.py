715d37a9027a28232114d6381ecad5a13c1012aa
sozoia <zfzg2232@gmail.com>
2023-12-04 00:12:20 +0300

    del main

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -10,12 +10,3 @@ def print_matrix_integer(matrix=[[]]):
                 sep = " " if k != matrix[j][-1] else ""
                 print("{:d}".format(k), end="{}".format(sep))
             print()
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
