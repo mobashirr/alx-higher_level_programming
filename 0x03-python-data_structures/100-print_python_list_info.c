b1ec16376a2d2d4284aee3ea7d83a9b0786f8537
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-05 01:19:21 +0300

    change the type of var

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,32 +1,24 @@
-#include <Python.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <Python.h>
 
-/**
- * print_python_list_info - print and modifypy list in c
- * @p: PyObject which is python list
- * Return: NULL
- */
 void print_python_list_info(PyObject *p)
 {
-	int size, allocated, i;
-	PyObject *obj;
-	PyListObject *ll;
+    Py_ssize_t pythonlist = PyList_Size(p),
+    alocated = ((PyListObject *)p)->allocated,
+    index = 0;
+    PyObject *object;
 
-	size = Py_SIZE(p);
-	allocated = ((PyListObject *)p)->allocated;
+    /*size of the object */
+    printf("[*] Size of the Python List = %zd\n", pythonlist);
+    /* memory allocated */
+    printf("[*] Allocated = %zd\n", alocated);
 
-	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d", allocated);
-
-	for (i = 0; i < size; i++)
-	{
-		printf("Element %d: ", i);
-
-		obj = PyList_GetItem(p, i);
-		ll = (PyListObject *)obj;
-		printf("%s\n", ll->ob_base.ob_base.ob_type);
-	}
-
+    /*print the elements of an opject*/
+    for (; index < pythonlist; index++)
+    {
+        object = PyList_GET_ITEM(p, index);
+        printf("Element %zd: %s\n", index, object->ob_type->tp_name);
+    }
 }
