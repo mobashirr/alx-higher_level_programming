7ef52f69f093141ad7085c03d966aed2079dd9f5
sozoia <zfzg2232@gmail.com>
2023-12-04 13:58:25 +0300

    betty style

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
+/**
+ * rev - reverse array
+ * @arr: array of int
+ * @len: length of array
+*/
 void rev(int **arr, int len)
 {
-	int i,j,k;
+	int i, j, k;
+
 	k = len / 2;
 
-	for(i = 0; i < k; i++)
+	for (i = 0; i < k; i++)
 	{
 		j = *arr[i];
 		*arr[i] = *arr[len];
@@ -14,43 +20,51 @@ void rev(int **arr, int len)
 		--len;
 	}
 }
-
+/**
+ * count - count length of linked list
+ * @head: head of list
+ * Return: length
+*/
 int count(listint_t *head)
 {
 	listint_t *temp = NULL;
+	int i = 0;
 
-	if(!head)
-		return(0);
+	if (!head)
+		return (0);
 
 	temp = head;
 
-	int i = 0;
-	while(temp)
+	while (temp)
 	{
-		if(temp->next)
+		if (temp->next)
 			temp = temp->next;
 		else
 			temp = NULL;
 		++i;
 	}
 	/*printf("len=%d\n",i); */
-	return(i);
+	return (i);
 }
-
+/**
+ * is_palindrome - check for palindrome linked list
+ * @head: head of list
+ * Return: 1 if palindrome of 0 if not
+*/
 int is_palindrome(listint_t **head)
 {
-	int len = 0, *arr,i = 0,j;
+	int len = 0, *arr, i = 0, j;
 	int pal = 0;    /*palndrome*/
 	listint_t *temp = NULL;
 
 	if (!(*head))
-		return(1);
+		return (1);
 
 	temp = (*head);
 	len = count(temp);
-	arr = malloc(sizeof(int) * (len / 2) );
+	arr = malloc(sizeof(int) * (len / 2));
 
-	while(i < len/2 && temp)
+	while (i < len / 2 && temp)
 	{
 		arr[i] = temp->n;
 		if (temp->next)
@@ -66,15 +80,15 @@ int is_palindrome(listint_t **head)
 
 	for (j = 0; temp; j++)
 	{
-			if(temp->n != arr[j])
-			{
-				pal = 1;
-				break;
-			}
-		if(temp->next)
+		if (temp->n != arr[j])
+		{
+			pal = 1;
+			break;
+		}
+		if (temp->next)
 			temp = temp->next;
 		else
 		temp = NULL;
 	}
-	return(pal);
+	return (pal);
 }
