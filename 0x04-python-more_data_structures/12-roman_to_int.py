0035e16ed9d282c9db55e0a783ab622841768c21
sozoia <zfzg2232@gmail.com>
2023-12-07 00:50:14 +0300

    style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
            "D": 500, "M": 1000}
     sub = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
-    
+
     res = 0
     i = 0
 
@@ -20,6 +20,7 @@ def roman_to_int(roman_string):
             i += 1
 
     return res
+
 
 '''
 roman_number = "X"
