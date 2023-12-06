c05b7082a429394a75bf36fb66d1adca47740a33
sozoia <zfzg2232@gmail.com>
2023-12-06 22:28:50 +0300

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+   
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
+
+
+'''
+# Test the function with the provided example
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
+'''
