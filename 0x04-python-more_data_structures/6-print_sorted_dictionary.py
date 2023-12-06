3f21e050d8eea27369ec57c3fd1358729aa9438a
sozoia <zfzg2232@gmail.com>
2023-12-06 22:13:23 +0300

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def number_keys(a_dictionary):
+
+    num = 0
+    for i in a_dictionary:
+        num += 1
+    return (num)
+'''
+a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
+nb_keys = number_keys(a_dictionary)
+print("Number of keys: {:d}".format(nb_keys))
+'''
