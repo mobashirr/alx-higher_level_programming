be6d9bc733581c813325f0f6700e9a610788a958
sozoia <zfzg2232@gmail.com>
2023-12-06 21:59:39 +0300

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+
+    new = [x for x in set_1 if x in set_2]
+    return (new)
+
+
+'''
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
+'''
