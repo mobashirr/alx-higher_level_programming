bb393d852a3b648b27224e420415c1f636b4c154
sozoia <zfzg2232@gmail.com>
2023-12-06 21:52:54 +0300

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+
+    new = [0]
+    for i in my_list:
+        if i not in new[1:]:
+            new.append(i)
+            new[0] = new[0] + i
+    return(new[0])
+
+
+'''
+#test case:
+un = [1, 2, 3, 1, 4, 2, 5]
+uniq_add(un)
+'''
