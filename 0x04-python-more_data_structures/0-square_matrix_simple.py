e3326afc9ff7efbb034dad34ebffd8b28d71ba76
sozoia <zfzg2232@gmail.com>
2023-12-06 21:16:24 +0300

    set files

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from math import pow
+def square_matrix_simple(matrix=[]):
+
+    list = [[],[],[]]
+    for i in range(0,len(matrix)):
+        list[i] = [int(pow(x,2)) for x in matrix[i]]
+    return (list)
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
