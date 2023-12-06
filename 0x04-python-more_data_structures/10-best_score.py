854bd13dcb85e102b599148d38443bdc4f0ee1d0
sozoia <zfzg2232@gmail.com>
2023-12-06 23:18:55 +0300

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+def best_score(a_dictionary):
+
+    if not a_dictionary:
+        return None
+
+    best = 0
+    for i in a_dictionary:
+        num = int(a_dictionary[i])
+        if num > best:
+            best = num
+            key = i
+
+    return(key)
+
+
+'''
+# test case:
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
+'''
