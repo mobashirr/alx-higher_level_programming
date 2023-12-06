ed8436bd58fe18e0008f1a251bcadf3cdbb1f5c2
sozoia <zfzg2232@gmail.com>
2023-12-06 22:20:42 +0300

    style

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -7,8 +7,9 @@ def number_keys(a_dictionary):
         num += 1
     return (num)
 
+
 '''
-a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level", "new": None}
+a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level"}
 nb_keys = number_keys(a_dictionary)
 print("Number of keys: {:d}".format(nb_keys))
 '''
