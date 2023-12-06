1123df01c39aab63273c0bedffc79790c00c6468
sozoia <zfzg2232@gmail.com>
2023-12-06 21:16:55 +0300

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -7,7 +7,7 @@ def square_matrix_simple(matrix=[]):
         list[i] = [int(pow(x,2)) for x in matrix[i]]
     return (list)
 
-
+'''
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
@@ -17,3 +17,4 @@ matrix = [
 new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
 print(matrix)
+'''
