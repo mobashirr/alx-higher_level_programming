2c3edc4d613cbffb8c59c46f0f7bb5b4fe1f06c2
sozoia <zfzg2232@gmail.com>
2023-12-06 23:54:33 +0300

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    if not roman_string:
+        return None
+
+    dic = {"I":1, "V":5, "X":10, "L":50, "C":100,
+           "D":500, "M":1000}
+    res = 0
+
+    for i in range(len(roman_string)):
+        if roman_string[i] in dic:
+            if dic[roman_string[i]] >= dic[roman_string[++i]]:
+                res += dic[roman_string[i]]
+            else:
+                res -= dic[roman_string[i]]
+    return res
+
+
+'''
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+'''
