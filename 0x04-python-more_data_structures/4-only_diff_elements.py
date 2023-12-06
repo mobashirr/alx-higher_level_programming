d33707da287dc28755a57487c38570770e65b111
sozoia <zfzg2232@gmail.com>
2023-12-06 22:07:04 +0300

    set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -5,8 +5,3 @@ def only_diff_elements(set_1, set_2):
     new = [x for x in set_1 if x not in set_2]
     new += [x for x in set_2 if x not in set_1]
     return new
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-od_set = only_diff_elements(set_1, set_2)
-print(sorted(list(od_set)))
