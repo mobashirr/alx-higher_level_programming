92682f30c8a56882eb35ece9b7af1a2ffcd0cf4b
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-18 19:29:16 +0000

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,7 +12,7 @@ def safe_print_list(my_list=[], x=0):
             else:
                 print()
                 break
-        except (r):
+        except (IndexError):
             print()
             break
     return i
