f558112aff32b5959d417bdb01a9625376b0b02a
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-18 21:43:02 +0000

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,26 +4,9 @@ import sys
 def safe_print_integer_err(value):
 
     try:
-        print("{:d}".format(value))
+        _value = "{:d}".format(value)
+        print(_value)
         return True
     except (TypeError, ValueError, NameError) as erorr:
-        sys.stderr.write(f"Exception:{erorr}")
+        sys.stderr.write(f"Exception: {erorr}\n")
         return False
-
-
-'''
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-'''
