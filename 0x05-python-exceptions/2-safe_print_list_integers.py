a8fa3364fcc3fc12ecf7a574439bdb820b713b60
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-18 21:56:28 +0000

    another sulotion

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,6 +2,23 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     ''' pritn first x elements of my_list: '''
+    count = 0
+
+    try:
+        for i in range(x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+    except IndexError:
+        pass
+
+    print()  # Print a newline after all integers
+    return count
+
+
+'''
+def safe_print_list_integers(my_list=[], x=0):
+
     i = 0   #i is idx for list
     ex = 0   #but we only return num of printed elements
 
@@ -16,3 +33,4 @@ def safe_print_list_integers(my_list=[], x=0):
 
     print()
     return i - ex
+'''
