328d8c63de4ea744a1aecabee87ad23a36d81193
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-18 21:35:00 +0000

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError, NameError) as erorr:
+        sys.stderr.write(f"Exception: {erorr}")
+        return False
