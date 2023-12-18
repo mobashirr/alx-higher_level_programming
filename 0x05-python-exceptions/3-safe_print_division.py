0ab041c5c71fdf876c2b0e1b687cfe57a58a25df
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-18 19:43:46 +0000

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def safe_print_division(a, b):
+
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("{}".format(res))
+    return res
