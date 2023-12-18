452935bbea835fb2785e9f09dda7e48ea4146171
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-18 19:36:16 +0000

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,13 +3,16 @@
 def safe_print_list_integers(my_list=[], x=0):
 
     i = 0
+    ex = 0  #we use i as index for list but we only return number of printed char
+
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
             i += 1
         except (TypeError, ValueError):
             i += 1
+            ex += 1 # we skiped element from the list
         except IndexError:
-            print()
             break
-    return i
+    print()
+    return i - ex
