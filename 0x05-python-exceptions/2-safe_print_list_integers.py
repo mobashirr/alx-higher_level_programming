43d1b8c45aea71f269f27f4ae12a3cd80bced8f8
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-18 21:54:05 +0000

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-
+    ''' pritn first x elements of my_list: '''
     i = 0   #i is idx for list
     ex = 0   #but we only return num of printed elements
 
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
-            i += 1
         except (TypeError, ValueError):
-            i += 1
-            ex += 1 # we skiped element from the list
+            ex += 1  # we skipped an element from the list
         except IndexError:
             break
+        i += 1
+
     print()
     return i - ex
