77f07d502fdb157b920b878c00c8fe2a6c0f40dd
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-18 21:46:40 +0000

    style

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
 
     try:
+        # some cases were value is not int
+        # but its a digit so we need to format first
         _value = "{:d}".format(value)
         print(_value)
         return True
