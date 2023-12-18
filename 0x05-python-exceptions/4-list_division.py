5aab174f0de7898a2bcc9eb1989052a397637d16
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-18 21:05:10 +0000

    del main

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -25,16 +25,3 @@ def list_division(my_list_1, my_list_2, list_length):
             print("Out of range")
 
     return res
-
-# Example usage
-my_list_1 = [10, 8, 4]
-my_list_2 = [2, 4, 4]
-result = list_division(my_list_1, my_list_2, max(len(my_list_1), len(my_list_2)))
-print(result)
-
-print("--")
-
-my_list_1 = [10, 8, 4, 4]
-my_list_2 = [2, 0, "H", 2, 7]
-result = list_division(my_list_1, my_list_2, max(len(my_list_1), len(my_list_2)))
-print(result)
