25f2f7ff2379c1d6e649cb3bd559b2b7a7f848a6
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-19 21:51:36 +0000

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,53 @@
 #!/usr/bin/python3
+
+'''task 6'''
+
+
+class Square:
+    '''task 6'''
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set the size."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """Getter method to retrieve the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter method to set the position."""
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or any(i < 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """Public instance method to calculate the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Public instance method to print the square with position."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
