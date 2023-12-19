6ce38c03b39a3e44292fe46f96c17a550f06b947
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-12-19 20:30:04 +0000

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-'''this is square class taht contain size'''
+'''this is square class that contain size'''
 
 
 class Square:
+    '''this class only contain size'''
 
     def __init__(self, size=0):
         self.size = size
