025047d8ce62904cb82c8b2dfc27252d50f42b68
sozoia <zfzg2232@gmail.com>
2023-12-30 20:18:05 +0300

    del des

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
 
-'''Why a getter and setter?
-
-Reminder: size is a private attribute.
-We did that to make sure we control the type and value.
-Getter and setter methods are not 100% Python, but more OOP.
-With them, you will be able to validate the assignment of a private attribute
-and also define how getting the attribute value will be available
-from outside - by copy?
-by assignment? etc. Also, adding type/value validation in
-the setter will centralize the logic,
- since you will do it in only one place. '''
-
-
 class Square:
     '''task 4'''
 
