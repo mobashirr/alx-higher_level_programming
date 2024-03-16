bfc3235f7262800c9a72474222efaf4879b687c6
sozoia <zfzg2232@gmail.com>
2024-03-16 19:40:13 +0300

    from 0 - 4

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+    """Check if an object is an instance of a class"""
+    return type(obj) == a_class
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+
+def is_kind_of_class(obj, a_class):
+    """Check if an object is an instance of a class"""
+    return isinstance(obj, a_class)
