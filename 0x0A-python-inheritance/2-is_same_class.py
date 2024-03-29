1ed5bbd45ce1e1e1d9a09e6b870d77e1c999ba98
sozoia <zfzg2232@gmail.com>
2024-03-29 23:28:42 +0300

    doc

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+'''Module for is_same_class method.'''
+
 def is_same_class(obj, a_class):
     """Check if an object is exactly an instance of the specified class.
 
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+'''Module for is_kind_of_class method.'''
+
 def is_kind_of_class(obj, a_class):
     """Check if an object is an instance of a class"""
     return isinstance(obj, a_class)
