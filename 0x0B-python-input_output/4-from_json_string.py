d993f6ff29b3ce4788c369137b9a29e72917bd93
sozoia <zfzg2232@gmail.com>
2024-04-02 02:21:51 +0300

    style 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+'''from_json_string module'''
+
 import json
 
+
 def from_json_string(my_str):
+    '''Returns an object represented by a JSON string.'''
     return json.loads(my_str)
