12631577c3085d9482f4bea2095417c20cd7b1cb
sozoia <zfzg2232@gmail.com>
2024-04-02 02:11:30 +0300

    all

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+class Student:
+    """Class representing a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student with first name, last name, and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+import sys
+from os import path
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+# Check if the file exists
+if path.exists(filename):
+    # Load existing items from file
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+# Add arguments to the list
+items.extend(sys.argv[1:])
+
+# Save the list to the file
+save_to_json_file(items, filename)
