1a5f6763f48592ca8ae6de95f16dae862cafb7a2
sozoia <zfzg2232@gmail.com>
2024-04-02 02:25:32 +0300

    style 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
+'''load_from_json_file module'''
+
 import json
 
+
 def load_from_json_file(filename):
+    '''creates an object from a JSON file'''
     with open(filename, 'r') as file:
         return json.load(file)
