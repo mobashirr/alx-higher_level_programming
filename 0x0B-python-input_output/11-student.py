aa1e4f8733e0cf380e50b79a43719f7dd93722dc
sozoia <zfzg2232@gmail.com>
2024-04-02 03:39:10 +0300

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+'''Module for Student class.'''
+
+
+class Student:
+    """Class representing a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student with first name, last name, and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
