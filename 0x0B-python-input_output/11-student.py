01277cd48f3895dd297e8614c2959c00651c21ed
sozoia <zfzg2232@gmail.com>
2024-04-02 03:53:30 +0300

    names

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -20,7 +20,7 @@ class Student:
             return {attr: getattr(self, attr)
                     for attr in attrs if hasattr(self, attr)}
 
-    def reload_from_json(self, json_data):
-        for key, value in json_data.items():
+    def reload_from_json(self, json):
+        for key, value in json.items():
             if hasattr(self, key):
                 setattr(self, key, value)
