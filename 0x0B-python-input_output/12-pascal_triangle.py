7222e40aa565a515a4c319c475059d4051ac4171
sozoia <zfzg2232@gmail.com>
2024-04-02 04:21:38 +0300

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]  # first element of each row is always 1
+        for j in range(1, i):
+            # middle elements are sum of upper-left and upper-right elements
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # last element of each row is always 1
+        triangle.append(row)
+
+    return triangle
