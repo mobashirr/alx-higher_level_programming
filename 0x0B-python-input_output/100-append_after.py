52052b1b5b24ac45f857f461a7b55debe35b3c6e
sozoia <zfzg2232@gmail.com>
2024-04-02 03:56:47 +0300

    adv 1

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+'''module for append_after func '''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''append after certian string'''
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
+        file.truncate()
