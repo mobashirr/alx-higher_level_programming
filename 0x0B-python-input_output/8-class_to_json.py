6592aad8df4fcce6ffd69554066bed975f337468
sozoia <zfzg2232@gmail.com>
2024-04-02 03:27:19 +0300

    DOC 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+'''Module for class_to_json method.'''
+
+
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure
        (list, dictionary, string, integer, and boolean) for JSON serialization
