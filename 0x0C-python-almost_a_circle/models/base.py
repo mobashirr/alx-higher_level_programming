27226a77494c25ed8e2ff73a3d2471f94d731fba
sozoia <zfzg2232@gmail.com>
2024-04-03 17:04:13 +0300

    create base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+'''base class mod'''
+
+class base():
+    '''base class'''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        base.__nb_objects += 1
+        if id is not None:
+            self.id = id
+        else:
+            self.id = base.__nb_objects
