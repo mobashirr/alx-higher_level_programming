bbc38b465e319366c8d62c201783251ebf660ce5
sozoia <zfzg2232@gmail.com>
2024-04-03 20:04:41 +0300

    back to priavte

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,10 +10,10 @@ class Rectangle(Base):
     '''rectangle class'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
