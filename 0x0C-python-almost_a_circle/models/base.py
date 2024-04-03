f1dd803c5d6702a871d6e7b6d12d8af5a43ed776
sozoia <zfzg2232@gmail.com>
2024-04-03 17:08:25 +0300

    fix name

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,13 +2,15 @@
 
 '''base class mod'''
 
-class base():
-    '''base class'''
-    __nb_objects = 0
 
+class Base():
+    '''base class'''
+
+    __nb_objects = 0
     def __init__(self, id=None):
-        base.__nb_objects += 1
+        
         if id is not None:
             self.id = id
         else:
-            self.id = base.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
