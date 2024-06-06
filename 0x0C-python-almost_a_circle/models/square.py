a48135d86b5beab41df38e10204823f5ceeb10cb
sozoia <zfzg2232@gmail.com>
2024-06-06 18:44:12 +0300

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,18 @@ class Square(Rectangle):
         '''assign the values with setter methods'''
         super().__init__(size, size, x, y, id)
  
+    @property
+    def size(self):
+        '''size getter'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''size setter'''        
+        self.width = value
+
     def __str__(self):
         '''return the string representation of the object'''
         return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y, self.width)
+
+
