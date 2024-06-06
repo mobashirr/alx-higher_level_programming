8d0fac87b466c45f69c76ebd3ad14b28261cff79
sozoia <zfzg2232@gmail.com>
2024-06-06 17:50:18 +0300

    task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,7 @@ class Rectangle(Base):
     '''rectangle class'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''assign the values with setter methods'''
         self.width = width
         self.height = height
         self.x = x
@@ -24,7 +25,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         '''width setter'''
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value <= 0:
             raise ValueError('width must be > 0')
@@ -39,7 +40,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         '''height setter'''
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
         elif value <= 0:
             raise ValueError('height must be > 0')
@@ -54,7 +55,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         '''x setter'''
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError('x must be an integer')
         elif value < 0:
             raise ValueError('x must be >= 0')
@@ -69,7 +70,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         '''y setter'''
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError('y must be an integer')
         elif value < 0:
             raise ValueError('y must be >= 0')
