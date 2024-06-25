f581433d5b6785ac2f1b34aff1535f4c95c9b73f
sozoia <zfzg2232@gmail.com>
2024-06-25 23:45:09 +0300

    task 3

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- this script do:
+-- lsit all tables of given database name
+USE `dbname_placeholder`;  -- Placeholder for database name
+SHOW TABLES;    -- Query to list all tables in the current database
