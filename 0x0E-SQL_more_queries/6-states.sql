29cbee3b90ac1387675773715d2d59e804223c99
sozoia <zfzg2232@gmail.com>
2024-06-27 23:15:16 +0300

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- create database and add new table to it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256),
+    PRIMARY KEY (id)
+);
