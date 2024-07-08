cc0178c2b41ad321bc616dd62214be62950f6b15
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 16:06:34 +0000

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+function isNumeric (value) {
+  return !isNaN(Number(value));
+}
+
+const messege = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const { argv } = require('node:process');
+
+if (isNumeric(argv[2])) {
+  // if the given arg is valid number:
+  for (let i = 0; i < Number(argv[2]); i++) {
+    console.log(messege[0]);
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
