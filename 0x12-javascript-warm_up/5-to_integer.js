2f530d6c8ed87dde36ee960b40b5ddc5145507f3
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 15:49:29 +0000

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+function isNumeric (value) {
+  return !isNaN(Number(value));
+}
+
+// parameters of the program
+const { argv } = require('node:process');
+
+if (isNumeric(argv[2])) {
+  console.log('My number: ' + argv[2]);
+} else {
+  console.log('Not a number');
+}
