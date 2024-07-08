abd7479b1cdf31c16c7ed2e05496c9cd293f4b0e
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 21:55:16 +0000

    task 11 second max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,17 +2,21 @@
 
 const { argv } = require('node:process');
 
-if (argv.length <= 2) { // No arguments provided
+if (argv.length <= 3) { // No arguments or only one argument provided
   console.log(0);
 } else {
   const numbers = argv.slice(2).map(Number); // Convert arguments to numbers
   let max = -Infinity;
+  let secondMax = -Infinity;
 
   for (let num of numbers) {
     if (num > max) {
+      secondMax = max;
       max = num;
+    } else if (num > secondMax && num < max) {
+      secondMax = num;
     }
   }
 
-  console.log(max);
+  console.log(secondMax);
 }
