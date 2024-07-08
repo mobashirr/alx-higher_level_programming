d390957e04a3e39fc7a710f621cc4c53132f7f38
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 15:36:44 +0000

    task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,9 +4,6 @@
 const { argv } = require('node:process');
 
 switch (argv.length) {
-  case 2:
-    console.log('No argument');
-    break;
   default:
     console.log(process.argv[3 - 1] + ' is ' + process.argv[4 - 1]);
 }
