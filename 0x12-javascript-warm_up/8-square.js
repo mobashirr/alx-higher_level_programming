789b65fc2564f4b1cf7038abbb33c87b796da6ec
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 20:58:03 +0000

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+function isNumeric (value) {
+  return !isNaN(Number(value));
+}
+
+const { argv } = require('node:process');
+
+if (isNumeric(argv[2])) {
+  for (let i = 0; i < Number(argv[2]); i++) {
+    console.log('#'.repeat(argv[2]));
+  }
+} else {
+  console.log('Missing size');
+}
