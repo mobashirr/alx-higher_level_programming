acc5f8dc95448c510adef4b645913fe1bbdecd05
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 21:54:30 +0000

    task 11 without var + fix logic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,21 +2,17 @@
 
 const { argv } = require('node:process');
 
-if (argv.length <= 3) { // No argument or only one argument
+if (argv.length <= 2) { // No arguments provided
   console.log(0);
 } else {
   const numbers = argv.slice(2).map(Number); // Convert arguments to numbers
   let max = -Infinity;
-  let secondMax = -Infinity;
 
   for (let num of numbers) {
     if (num > max) {
-      secondMax = max;
       max = num;
-    } else if (num > secondMax && num < max) {
-      secondMax = num;
     }
   }
 
-  console.log(secondMax);
+  console.log(max);
 }
