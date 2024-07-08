6ea0a73a9c355825a3fe0354b0e0ca5050c8137a
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 15:35:42 +0000

    task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+// print two arg given to the program 
+// parameters of the program
+const { argv } = require('node:process');
+
+switch (argv.length) {
+  case 2:
+    console.log('No argument');
+    break;
+  default:
+    console.log(process.argv[3 - 1] + ' is ' + process.argv[4 - 1]);
+}
