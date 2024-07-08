00432b54fc338440955b9ed70e2117b0dc0901c1
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 21:13:47 +0000

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// factorial with js:
+
+function factorial (number) {
+  if (number <= 0 || isNaN(number)) {
+    return (1);
+  } else {
+    return (number * factorial(number - 1));
+  }
+}
+
+const { argv } = require('node:process');
+
+console.log(factorial(argv[2]));
