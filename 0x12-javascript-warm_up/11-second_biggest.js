571f30680aedca8f9f40e18296ba1121f76e8855
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 21:48:56 +0000

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv.length < 4) {
+  console.log(0);
+} else {
+  var big = Number(argv[2]);
+  for (let i = 2; i < argv.length; i++) {
+    if (isNaN(argv[i])) {
+      continue;
+    } else if (big < Number(argv[i])) {
+      big = Number(argv[i]);
+    }
+  }
+  console.log(big);
+}
