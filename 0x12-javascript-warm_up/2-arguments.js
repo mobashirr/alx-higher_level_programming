5f78df68c427624b597f05dbde2122125b6e3516
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 14:07:58 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+switch (argv.length) {
+  case 2:
+    console.log('No argument');
+    break;
+  case 3:
+    console.log('Argument found');
+    break;
+  default:
+    console.log('Arguments found');
+}
