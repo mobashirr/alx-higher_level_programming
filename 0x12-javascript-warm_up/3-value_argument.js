f990b2833f96835ff1fe1f6063546ec44c644844
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 15:31:41 +0000

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 
-
 // parameters of the program
 const { argv } = require('node:process');
 
-let length = argv.reduce((acc, _) => acc + 1, 0);
+const length = argv.reduce((acc, _) => acc + 1, 0);
 
 switch (length) {
   case 2:
     console.log('No argument');
     break;
   case 3:
-    console.log(process.argv[3-1]);
+    console.log(process.argv[3 - 1]);
     break;
   default:
     console.log('Arguments found');
