a5de7db47915fb964781e599e3cdda2e2618484c
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-07-08 13:49:34 +0000

    setup

--- 0x12-javascript-warm_up/setup.sh
+++ 0x12-javascript-warm_up/setup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
 # install node 14
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+
+# update npm
+npm install -g npm@latest
 
 # install semi-standard
+# its tool used to check for standerd js of writing code
+# run using the command -> semistandard file.js
 sudo npm install semistandard --global
