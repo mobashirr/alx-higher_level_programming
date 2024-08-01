e66e8549573e55f392e7a915f2a9e787c81d42d0
sozoia <zfzg2232@gmail.com>
2024-08-01 22:52:29 +0300

    state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""Contains the State class and Base instance"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Represents the state table in the database"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
