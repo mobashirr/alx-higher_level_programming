7a4be85da2c1813ea5025e8c8e20ce1556aa63b8
sozoia <zfzg2232@gmail.com>
2024-08-01 22:28:11 +0300

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+
+'''use mysqldb to fetch data from mysql server'''
+
+from sys import argv
+import MySQLdb
+
+def main():
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    # conect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cursor = db.cursor()
+
+    # Execute the query
+    cursor.execute("SELECT id, name FROM cities ORDER BY id ASC")
+
+    # Fetch all the results
+    rows = cursor.fetchall()
+
+    # Print each row
+    for row in rows:
+        print(row)
+
+    # Close the cursor and the connection
+    cursor.close()
+    db.close()
+
+if len(argv) > 3:
+    main()
+else:
+    print(f"USAGE {argv[0]} mysql_username mysql_password  database_name")
