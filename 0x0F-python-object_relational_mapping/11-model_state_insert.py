74a3f01bcefd76507d1763dee116c19707d051c5
sozoia <zfzg2232@gmail.com>
2024-08-01 23:56:41 +0300

    task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: ./11-model_state_insert.py <mysql_username> <mysql_password> <database_name>")
+        return
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Create engine and connect to the MySQL server
+    engine = create_engine(f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}')
+    
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    
+    # Create a Session
+    session = Session()
+
+    # Create a new State object
+    new_state = State(name='Louisiana')
+
+    # Add the new State object to the session
+    session.add(new_state)
+
+    # Commit the session to save the new State object to the database
+    session.commit()
+
+    # Print the new state's id
+    print(new_state.id)
+
+    # Close the session
+    session.close()
+
+if __name__ == "__main__":
+    main()
