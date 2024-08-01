ea5ca1651c9c71709fd887907535f9755dbfa991
sozoia <zfzg2232@gmail.com>
2024-08-01 21:54:32 +0300

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 
+'''this module intract with mysql db using mysqldb module'''
+
+from sys import argv
 import MySQLdb
-import sys
 
-if __name__ == "__main__":
-    try:
-        # Get command line arguments
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
+def main():
+    ''''main logic'''
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
 
-        # Connect to the database
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
+    # conect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cursor = db.cursor()
 
-        # Execute the query with filter
-        cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # Execute the query
+    cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-        # Fetch all the results
-        rows = cursor.fetchall()
+    # Fetch all the results
+    rows = cursor.fetchall()
 
-        # Print each row
-        for row in rows:
-            print(row)
+    # Print each row
+    for row in rows:
+        print(row)
 
-    except MySQLdb.Error as e:
-        print(f"Error {e.args[0]}: {e.args[1]}")
-    finally:
-        if cursor:
-            cursor.close()
-        if db:
-            db.close()
+    # Close the cursor and the connection
+    cursor.close()
+    db.close()
+
+if len(argv) > 3:
+    main()
+else:
+    print(f"USAGE {argv[0]} mysql_username mysql_password  database_name")
